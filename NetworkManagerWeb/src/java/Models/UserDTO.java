b0d53a8dd4ee307ff2c0257a287c12e4e056b06d
NguyenThanh111 <mycrush146@gmail.com>
2026-06-03 15:11:10 +0700

    LoginService
     - UserDAO, UserDTO(done) - plz check again !?!
    - UserRoleDTO, UserRoleDAO (not complete)

---
+++ NetworkManagerWeb/src/java/Models/UserDTO.java
@@ -0,0 +1,84 @@
+
+package Models;
+
+public class UserDTO {
+    private int userId;
+    private String userName;
+     private String password;
+     private String fullName;
+     private String email;
+     private String role;
+     private boolean status;
+
+    public UserDTO(int userId, String userName, String password, String fullName, String email, String role, boolean status) {
+        this.userId = userId;
+        this.userName = userName;
+        this.password = password;
+        this.fullName = fullName;
+        this.email = email;
+        this.role = role;
+        this.status = status;
+    }
+
+    public UserDTO() {
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+     
+     
+    
+}
